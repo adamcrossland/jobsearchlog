9ed792b8284ec6e4006a5fe225bff8afbe9b7430
Adam Crossland <adam.crossland@gmail.com>
2025-01-21 17:11:28 -0500

    remove test data and focus on new employer field after hitting Add+

--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,41 @@ import Alpine from 'alpinejs'
 // make Alpine on window available for better DX
 window.Alpine = Alpine
 
+interface DataItem {
+    Value: string;
+    BeingEdited: boolean;
+}
+
+interface JobSearchItem {
+    Id: number;
+    StartDate: DataItem;
+    UpdatedDate: DataItem;
+    EmployerName: DataItem;
+}
+
+let JobSearchData: JobSearchItem[] = [
+];
+
 Alpine.store('jobsearchdata', {
-    searches: [
-        {
-            StartDate: "10/15/2024",
-            UpdateDate: "11/01/2024",
-            EmployerName: "National Grid"
-        }
-    ],
+    searches: JobSearchData,
+
     get empty() {
         return this.searches.length === 0
-    }
+    },
+
+    newSearchRow() {
+        let newRow = {
+            Id: ++this.nextRowId,
+            StartDate: { Value: new Date().toDateString(), BeingEdited: true },
+            UpdatedDate: { Value: new Date().toDateString(), BeingEdited: true },
+            EmployerName: { Value: "", BeingEdited: true }
+        }
+        this.searches.unshift(newRow);
+        Alpine.nextTick(() => {
+            document.querySelector("#searches-table tbody tr td + td input").focus();
+        });
+    },
+    nextRowId: 0
 });
 
 Alpine.start()
