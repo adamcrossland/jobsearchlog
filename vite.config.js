c83a459eee808801eed9c2967d64de3e8d9e13de
Adam Crossland <adam.crossland@gmail.com>
2025-12-20 11:16:33 -0500

    [make-pwa] switch to using vita-pwa
    This makes it a lot easier to get the required manifest files to
    match up with the vite-generated file names

---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import { VitePWA } from 'vite-plugin-pwa'
+export default {
+    plugins: [
+        VitePWA({
+            manifest: {
+                name: 'Job Search Log',
+                short_name: 'Job Search Log',
+                start_url: '/',
+                background_color: '#ffffff',
+                theme_color: '#000000',
+                display: 'standalone',
+                icons: [
+                    { src: 'jsl_192x192.png', sizes: '192x192', type: 'image/png' },
+                    { src: 'jsl_512x512.png', sizes: '512x512', type: 'image/png' },
+                    { src: 'android-chrome-192x192.png', size: '192x192', type: 'image/png' },
+                    { src: 'android-chrome-512x512.png', size: '512x512', type: 'image/png' },
+                    { src: 'apple-touch-icon.png', size: '180x180', type: 'image/png' },
+                    { src: 'favicon-16x16.png', size: '16x16', type: 'image/png' },
+                    { src: 'favicon-32x32.png', size: '32x32', type: 'image/png' },
+                    { src: 'favicon.ico', size: '48x48', type: 'image/ico' },
+                ]
+            },
+            workbox: {
+                globPatterns: ['**/*.{js,css,html,ico,png,svg,webmanifest}']
+            }
+        })
+    ]
+}
